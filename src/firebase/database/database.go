aaa052e6286f58e83baee345ed3e5e5b1b1365ee
kurikei <kurikei@gmail.com>
2018-10-15 19:00:34 +0900

    firebase realtime database からデータを取得する

---
+++ src/firebase/database/database.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+type Message struct {
+	Name          string `json:"name"`
+	ProfilePicUrl string `json:profilePicUrl`
+	Text          string `json:"text"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	opt := option.WithCredentialsFile(os.Getenv("GCLOUD_CRENTIAL_FILE_PATH"))
+	config := &firebase.Config{
+		DatabaseURL: os.Getenv("FIREBASE_DATASTORE_URL"),
+	}
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := app.Database(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var messages map[string]Message
+	err = client.NewRef("messages").Get(ctx, &messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for key, message := range messages {
+		log.Printf("%s: %+v\n", key, message)
+	}
+}
