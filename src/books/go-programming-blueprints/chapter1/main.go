d739bf79178292bbd3467c1491d96c71725b0367
kurikei <kurikei@gmail.com>
2018-10-16 16:03:45 +0900

    1.1.1 テンプレート を実装した

--- src/books/go-programming-blueprints/chapter1/main.go
+++ src/books/go-programming-blueprints/chapter1/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// http.HandleFunc に直接渡せるインターフェイス
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("template", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
