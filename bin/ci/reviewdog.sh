e68fe3cd0af7c00060986299a2543a6bbe21e7b3
kurikei <kurikei@gmail.com>
2018-10-23 16:08:13 +0900

    CI に golangci-lint を追加した

--- bin/ci/reviewdog.sh
+++ bin/ci/reviewdog.sh
@@ -7,3 +7,19 @@ if [ "$CI_PULL_REQUEST" = "" ]; then
 fi
 
 golint $(go list ./${SRC_PATH}/...) | eval reviewdog -f=golint $REVIEWDOG_ARG
+
+golangci-lint run \
+    --issues-exit-code 0 \
+    --out-format checkstyle \
+    --disable-all \
+    -E govet \
+    -E errcheck \
+    -E ineffassign \
+    -E interfacer \
+    -E unconvert \
+    -E misspell \
+    -E unparam \
+    -E nakedret \
+    -E prealloc \
+    ./${SRC_PATH}/... \
+    | eval reviewdog -name=golangci-lint -f=checkstyle $REVIEWDOG_ARG
